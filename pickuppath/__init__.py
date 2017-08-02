c098a9c40661ba75d41c10393a6ac5e1bf7e95c9
podhmo <ababjam61+github@gmail.com>
2017-08-03 04:03:28 +0900

    refine

--- pickuppath/__init__.py
+++ pickuppath/__init__.py
@@ -4,23 +4,22 @@ import os.path
 logger = logging.getLogger(__name__)
 
 
+def iter_parents(filename, current=None):
+    current = os.path.normpath(os.path.abspath(current or os.getcwd()))
+    while True:
+        yield os.path.join(current, filename)
+        if current == "/":
+            break
+        current, dropped = os.path.split(current)
+
+
 def pickup_path(filename, current=None, default=None):
     """pickupping the config file path
 
     start path = "/foo/bar/boo", filename = "config.ini"
     finding candidates are ["/foo/bar/boo/config.ini", "/foo/bar/config.ini", "/foo/config.ini", "/config.ini"]
     """
-    current = current or os.getcwd()
-    start_point = os.path.normpath(os.path.abspath(current))
-    current = start_point
-    candidates = []
-    while True:
-        candidates.append(os.path.join(current, filename))
-        if current == "/":
-            break
-        current, dropped = os.path.split(current)
-
-    for path in candidates:
+    for path in iter_parents(filename, current):
         logger.debug("check: %s", path)
         if os.path.exists(path):
             return path
