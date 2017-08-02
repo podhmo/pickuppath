b7ff0c6ed404b5eb671ccc64a623ae16ef8de63c
podhmo <ababjam61+github@gmail.com>
2017-08-02 21:56:45 +0900

    roughly implementation

--- pickuppath/__init__.py
+++ pickuppath/__init__.py
@@ -1,4 +1,26 @@
 # -*- coding:utf-8 -*-
 import logging
+import os.path
 logger = logging.getLogger(__name__)
 
+
+def pickup_path(start_path, filename, default=None):
+    """pickupping the config file path
+
+    start path = "/foo/bar/boo", filename = "config.ini"
+    finding candidates are ["/foo/bar/boo/config.ini", "/foo/bar/config.ini", "/foo/config.ini", "/config.ini"]
+    """
+    start_point = os.path.normpath(os.path.abspath(start_path))
+    current = start_point
+    candidates = []
+    while True:
+        candidates.append(os.path.join(current, filename))
+        if current == "/":
+            break
+        current, dropped = os.path.split(current)
+
+    for path in candidates:
+        logger.debug("check: %s", path)
+        if os.path.exists(path):
+            return path
+    return default
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ testing_extras = tests_require + []
 
 setup(
     name='pickuppath',
-    version='0.0',
+    version='0.1.0',
     description='pickup path',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
@@ -45,5 +45,7 @@ setup(
     tests_require=tests_require,
     test_suite="pickuppath.tests",
     entry_points="""
+    [console_scripts]
+    packuppath = pickuppath.__main__:main
 """
 )
